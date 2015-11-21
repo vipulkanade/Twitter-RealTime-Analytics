479df5ba162ffa0d54632a8dce60ff4b7042a709
Vipul Kanade <vipultechjoy@gmail.com>
2015-11-21 13:06:55 -0800

    Config Chnages

--- tweeterStatisticsAPI/config.js
+++ tweeterStatisticsAPI/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  pusher_app_id: "", //PUSHER_APP_ID
-  pusher_key: "", //PUSHER_APP_KEY
-  pusher_secret: "", //PUSHER_APP_SECRET
-  twitter_consumer_key: "", //TWITTER_CONSUMER_KEY
-  twitter_consumer_secret: "", //TWITTER_CONSUMER_SECRET
-  twitter_access_token_key: "", //TWITTER_ACCESS_TOKEN_KEY
-  twitter_access_token_secret: "", //TWITTER_ACCESS_TOKEN_SECRET
-  keywords: ["", "", ""] // enter keywords to track
+  pusher_app_id: "154790", //PUSHER_APP_ID
+  pusher_key: "1d38d329811cac9f5e78", //PUSHER_APP_KEY
+  pusher_secret: "5b2d8853b06d4b056622", //PUSHER_APP_SECRET
+  twitter_consumer_key: "4Z7rO92NcSWS79SVvwU1s89o1", //TWITTER_CONSUMER_KEY
+  twitter_consumer_secret: "73amhsI7dX7Z2dwtSUHNqhNtuQL7JeVYgvA0DKTOWnFBLQzamL", //TWITTER_CONSUMER_SECRET
+  twitter_access_token_key: "4271998153-F9pabbWK3jqoCKcPnBGaxsjPASxXevZKgpCTSwS", //TWITTER_ACCESS_TOKEN_KEY
+  twitter_access_token_secret: "KZaHpDRAM17Llt5mY1WgM5DTegtW0lIqN62xNME88G5Ym", //TWITTER_ACCESS_TOKEN_SECRET
+  keywords: ["FIFA", "LOVE", "WARS"] // enter keywords to track
 }
